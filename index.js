13fdfee4ab53a6d2c9ec7e5f18710f8cb4d9025c
nahidthenh <nhgroup18@gmail.com>
2021-12-06 16:54:02 +0600

    File

---
+++ index.js
@@ -0,0 +1,20 @@
+// Arrow Function
+
+const sum = (a, b) => {
+    const x = a + b;
+    const y = a - b;
+    return y, x;
+};
+
+// console.log(sum(112, 10)); 
+
+// Truthy / falsy value ----------------------
+let a = 0;
+
+if (a) {
+    // console.log('Truthy Value');
+} else {
+    // console.log('false Value');
+}
+
+// Ternary Operator -------------------------------
