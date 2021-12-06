56c9810ff3529c13018d1feb0819073dfe62408d
nahidthenh <nhgroup18@gmail.com>
2021-12-06 17:09:15 +0600

    Ternary operator

--- index.js
+++ index.js
@@ -18,3 +18,12 @@ if (a) {
 }
 
 // Ternary Operator -------------------------------
+//  if age less then 10 type should be 'Child'
+//  if age 18+ type should be 'Adult'
+//  if age 30+ then type shuld be ' You are Old'
+
+const age = 31;
+
+const type = (age >= 18 && age <= 30) ? 'Type is adult' : age <= 10 ? 'Type is Child' : 'You Are Old'
+
+console.log(type);
